07ecd56f38f092623a95b29f3c7a7c2c2b51e528
alexeyb7 <alexeyb7@gmail.com>
2024-04-29 17:18:44 +0300

    Improve DZ3

--- flask_dz3/flask_dz3/models_01.py
+++ flask_dz3/flask_dz3/models_01.py
@@ -1,3 +1,4 @@
+import nullable
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -6,11 +7,11 @@ class Book(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     bookname = db.Column(db.String(50), nullable = False)
     publicyear = db.Column(db.Integer, nullable = False)
-    copies = db.Column(db.Integer, nullable - False)
+    copies = db.Column(db.Integer, nullable = False)
     id_author = db.Column(db.Integer, db.ForeignKey('author.id'), nullable = False)
 
     def __repr__(self):
-        return f'Книга:{self.bookname}, Автор:{id_author}'
+        return f'Книга:{self.bookname}, Автор:{self.id_author}'
 
 
 class Author(db.Model):
